d5187272c6b720dbe68335a735b3a761c35d3117
Ajla El Tabari <ajla.eltabari@BitCamps-Mac-mini-65.local>
2015-06-15 10:36:29 +0200

    Initial commit

---
+++ src/ba/BITCamp/ajla/gitAndVersioning/MojaKlasa.java
@@ -0,0 +1,18 @@
+package ba.BITCamp.ajla.gitAndVersioning;
+
+public class MojaKlasa {
+	public String a;
+	public int b;
+	
+	/**
+	 * @param a
+	 * @param b
+	 */
+	public MojaKlasa(String a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	
+	
+}
